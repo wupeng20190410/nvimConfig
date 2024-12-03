8cb8b2f468e72e14017ebf7b8d08348101380ba6
lr <lr1180528@gmail.com>
2024-12-03 16:12:24 +0800

    reconfigure nvim-tree mapping

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -11,13 +11,11 @@ return {
                 end
 
                 -- custom mappings
-                -- Map "l" to open a file or folder
+                api.config.mappings.default_on_attach(bufnr)
                 vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
-                -- Optionally map "h" to go up one level (close folder)
-                vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close Directory"))
-                vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
                 vim.keymap.set('n','.',api.tree.change_root_to_node,opts('change_root_to_node'))
-                vim.keymap.set('n', '-', api.tree.change_root_to_parent, opts('Up'))
+
+
             end,
         })
     end
